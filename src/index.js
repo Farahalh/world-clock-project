bca0eebeede51ef60f41418e2f3d63164c9764cd
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-02-27 13:19:57 +0100

    Cleaned JS code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function updateTimeParis() {
+function updateTime() {
   let parisElement = document.querySelector("#paris");
   let parisDateElement = parisElement.querySelector(".date");
   let parisTimeElement = parisElement.querySelector(".time");
@@ -6,36 +6,25 @@ function updateTimeParis() {
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-}
 
-updateTimeParis();
-setInterval(updateTimeParis, 1000);
-
-function updateTimeSydney() {
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
   let sydneyTime = moment().tz("Australia/Sydney");
 
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
-}
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-updateTimeSydney();
-setInterval(updateTimeSydney, 1000);
-
-
-function updateTimeTokyo() {
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
   let tokyoTime = moment().tz("Asia/Tokyo");
 
   tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
-updateTimeTokyo();
-setInterval(updateTimeTokyo, 1000);
+updateTime();
+setInterval(updateTime, 1000);
