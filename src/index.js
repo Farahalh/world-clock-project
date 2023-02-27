e6c6ecbdaab896519b6e27eab10274658d844187
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-02-27 11:55:34 +0100

    Adding Moment Timezone to JS for single location

---
+++ src/index.js
@@ -0,0 +1,41 @@
+function updateTimeParis() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTimeParis();
+setInterval(updateTimeParis, 1000);
+
+function updateTimeSydney() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTimeSydney();
+setInterval(updateTimeSydney, 1000);
+
+
+function updateTimeTokyo() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTimeTokyo();
+setInterval(updateTimeTokyo, 1000);
